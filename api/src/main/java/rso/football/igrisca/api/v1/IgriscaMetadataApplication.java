829f3cc57ab69f0ade0cd6824a72347ac384829b
Rok Bosil <rb2600@student.uni-lj.si>
2022-01-05 16:11:40 +0100

    Added openApi

--- api/src/main/java/rso/football/igrisca/api/v1/IgriscaMetadataApplication.java
+++ api/src/main/java/rso/football/igrisca/api/v1/IgriscaMetadataApplication.java
@@ -1,8 +1,19 @@
 package rso.football.igrisca.api.v1;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+//TODO: change url to not localhost
+@OpenAPIDefinition(info = @Info(title = "Igrisca API", version = "v1",
+        contact = @Contact(email = "rb2600@student.uni-lj.si"),
+        license = @License(name = "dev"), description = "API for managing igrisca metadata."),
+        servers = @Server(url = "http://localhost:8080/"))
 @ApplicationPath("/v1")
 public class IgriscaMetadataApplication extends Application {
 
--- api/src/main/java/rso/football/igrisca/api/v1/resources/IgriscaMetadataResource.java
+++ api/src/main/java/rso/football/igrisca/api/v1/resources/IgriscaMetadataResource.java
@@ -1,6 +1,15 @@
 package rso.football.igrisca.api.v1.resources;
 
 import com.kumuluz.ee.cors.annotations.CrossOrigin;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.headers.Header;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import rso.football.igrisca.lib.IgriscaMetadata;
 import rso.football.igrisca.services.beans.IgriscaMetadataBean;
 
@@ -28,13 +37,28 @@ public class IgriscaMetadataResource {
     
     @Context
     protected UriInfo uriInfo;
-    
+
+    @Operation(description = "Get all igrisca metadata.", summary = "Get all metadata")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "List of igrisca metadata",
+                    content = @Content(schema = @Schema(implementation = IgriscaMetadata.class, type = SchemaType.ARRAY)),
+                    headers = {@Header(name = "X-Total-Count", description = "Number of objects in list")}
+            )})
     @GET
     public Response getIgriscaMetadata(){
         List<IgriscaMetadata> igriscaMetadata = igriscaMetadataBean.getIgriscaMetadataFilter(uriInfo);
         return Response.status(Response.Status.OK).entity(igriscaMetadata).build();
     }
 
+
+    @Operation(description = "Get all igrisca id.", summary = "Get all id igrisca")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "String igrisca id",
+                    content = @Content(
+                            schema = @Schema(implementation = String.class))
+            )})
     @GET
     @Path("/igriscaId")
     public Response getTrenerjiIdMetadata() {
@@ -44,9 +68,17 @@ public class IgriscaMetadataResource {
         return Response.status(Response.Status.OK).entity(trenerjiId).build();
     }
 
+    @Operation(description = "Get metadata for one igrisce.", summary = "Get metadata for one igrisce")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "Igrisce metadata",
+                    content = @Content(
+                            schema = @Schema(implementation = IgriscaMetadata.class))
+            )})
     @GET
     @Path("/{igriscaMetadataId}")
-    public Response getIgriscaMetadata(@PathParam("igriscaMetadataId") Integer igriscaMetadataId) {
+    public Response getIgriscaMetadata(@Parameter(description = "Metadata ID.", required = true)
+                                        @PathParam("igriscaMetadataId") Integer igriscaMetadataId) {
 
         IgriscaMetadata igriscaMetadata = igriscaMetadataBean.getIgriscaMetadata(igriscaMetadataId);
 
@@ -57,8 +89,18 @@ public class IgriscaMetadataResource {
         return Response.status(Response.Status.OK).entity(igriscaMetadata).build();
     }
 
+    @Operation(description = "Add igrisce metadata.", summary = "Add metadata")
+    @APIResponses({
+            @APIResponse(responseCode = "201",
+                    description = "Metadata successfully added."
+            ),
+            @APIResponse(responseCode = "400", description = "Bad request.")
+    })
     @POST
-    public Response createIgriscaMetadata(IgriscaMetadata igriscaMetadata) {
+    public Response createIgriscaMetadata(@RequestBody(
+            description = "DTO object with igrisca metadata.",
+            required = true, content = @Content(
+            schema = @Schema(implementation = IgriscaMetadata.class))) IgriscaMetadata igriscaMetadata) {
 
         if (igriscaMetadata.getName() == null) {
             return Response.status(Response.Status.BAD_REQUEST).build();
@@ -67,14 +109,27 @@ public class IgriscaMetadataResource {
             igriscaMetadata = igriscaMetadataBean.createIgriscaMetadata(igriscaMetadata);
         }
 
-        return Response.status(Response.Status.CONFLICT).entity(igriscaMetadata).build();
+        return Response.status(Response.Status.CREATED).entity(igriscaMetadata).build();
 
     }
 
+    @Operation(description = "Update metadata for on igrisce.", summary = "Update metadata")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "204",
+                    description = "Metadata successfully updated."
+            ),
+            @APIResponse(responseCode = "404", description = "Not found.")
+    })
     @PUT
     @Path("{igriscaMetadataId}")
-    public Response putIgriscaMetadata(@PathParam("igriscaMetadataId") Integer igriscaMetadataId,
-                                     IgriscaMetadata igriscaMetadata) {
+    public Response putIgriscaMetadata(@Parameter(description = "Metadata ID.", required = true)
+                                       @PathParam("igriscaMetadataId") Integer igriscaMetadataId,
+                                       @RequestBody(
+                                               description = "DTO object with igrisce metadata.",
+                                               required = true, content = @Content(
+                                               schema = @Schema(implementation = IgriscaMetadata.class)))
+                                               IgriscaMetadata igriscaMetadata) {
 
         igriscaMetadata = igriscaMetadataBean.putIgriscaMetadata(igriscaMetadataId, igriscaMetadata);
 
@@ -82,13 +137,26 @@ public class IgriscaMetadataResource {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
-        return Response.status(Response.Status.NOT_MODIFIED).build();
+        return Response.status(Response.Status.NO_CONTENT).build();
 
     }
 
+
+    @Operation(description = "Delete metadata for one igrisce.", summary = "Delete metadata")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "204",
+                    description = "Metadata successfully deleted."
+            ),
+            @APIResponse(
+                    responseCode = "404",
+                    description = "Not found."
+            )
+    })
     @DELETE
     @Path("{igriscaMetadataId}")
-    public Response deleteIgriscaMetadata(@PathParam("igriscaMetadataId") Integer igriscaMetadataId) {
+    public Response deleteIgriscaMetadata(@Parameter(description = "Metadata ID.", required = true)
+                                              @PathParam("igriscaMetadataId") Integer igriscaMetadataId) {
 
         boolean deleted = igriscaMetadataBean.deleteIgriscaMetadata(igriscaMetadataId);
 
